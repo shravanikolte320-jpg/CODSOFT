25d97dbaee304d899ceb3e9b7138b01813b78abb
shravanikolte320-jpg <shravanikolte320@gmail.com>
2026-05-16 13:51:36 +0530

    TASK_2

---
+++ CODSOFT_TASK_2/StudentGradeCalculator.java
@@ -0,0 +1,58 @@
+package com.game;
+
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int total = 0;
+
+        for (int i = 1; i <= 
+        	
+        		subjects; i++) {
+
+            System.out.print("Enter marks for subject " + i + ": ");
+            int marks = sc.nextInt();
+
+            total += marks;
+        }
+
+        double average = (double) total / subjects;
+
+        String grade;
+
+        if (average >= 90) {
+            grade = "A+";
+        }
+
+        else if (average >= 80) {
+            grade = "A";
+        }
+
+        else if (average >= 70) {
+            grade = "B";
+        }
+
+        else if (average >= 60) {
+            grade = "C";
+        }
+
+        else if (average >= 50) {
+            grade = "D";
+        }
+
+        else {
+            grade = "Fail";
+        }
+
+        System.out.println("\nTotal Marks: " + total);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
